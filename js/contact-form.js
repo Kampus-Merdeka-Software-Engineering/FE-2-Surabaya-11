d1658d4e482c8251eeee8f3f00d2e75b06cf89ce
andrianisaf <nisa.anisafitri@gmail.com>
2023-12-11 14:30:15 +0700

    clean comment

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,4 +1,33 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const prePayload = new FormData(form);
+    const payload = new URLSearchParams(prePayload);
+
+    console.log([...payload])
+
+    fetch('http://localhost:5500/api/contacts/addContact', {
+        method: "POST",
+        body: prePayload,
+    })
+        .then(res => res.json())
+        .then(data => {
+          console.log('Data from server:', data);
+      })
+        .catch(err => console.log(err));
+})
+
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    submitForm();
+});
+
 function submitForm() {
-  alert("Thank you for your message!");
-  window.location.href = "contact.html";
+    alert("Thank you for your message!");
+    window.location.href = "contact.html";
 }
+
